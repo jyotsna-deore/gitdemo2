bf055e103155675e7b9082abc114a814247232b5
jyotsna-deore <61134285+jyotsna-deore@users.noreply.github.com>
2020-02-17 12:11:23 +0530

    Update Test.java

--- githubAmit2/src/githubAmit2/Test.java
+++ githubAmit2/src/githubAmit2/Test.java
@@ -7,11 +7,15 @@ public class Test
 	{
 		System.out.println("new method'");
 	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Hello git hub");
-		
+		System.out.println("Hello new edit");
 		Test t= new Test();
 		t.m1();
+		
+		
 			
 	}
 }
